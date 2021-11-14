f9116f452c9303b1c35ee007ed31d15acb855b7c
Przemysław Padoł <padol19@gmail.com>
2021-11-14 19:10:52 +0100

    added file with passwords and updated index.php

---
+++ examplePassword.php
@@ -0,0 +1,5 @@
+<?php
+$databaseAddress = 'localhost';
+$databaseUsername = 'root';
+$databasePassword = '';
+$databaseName = 'pytania';
--- index.php
+++ index.php
@@ -1,3 +1,119 @@
+<?php
+
+//rozpoczęcie sesji php
+session_start();
+
+// połączenie z bazą danych
+require('examplePassword.php');
+$mysqli = mysqli_connect($databaseAddress, $databaseUsername, $databasePassword, $databaseName);
+
+//$mysqli->set_charset('utf8mb4');
+echo '<script type="text/javascript">console.log("połączono z bazą danych");</script>';
+
+// w przypadku sesji ustawionej na zmienną adminSession zwracamy panel admina
+if (isset($_SESSION['adminSession'])) {
+    return header('location: admin/scores.php');
+}
+
+// w przypadku id użytkownika zwracamy stronę użytkownika
+if (isset($_SESSION['userId'])) {
+    return header('location: user/scores.php');
+}
+
+// W przypadku zapytania post
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    switch ($_GET['action']) {
+
+            // w przypadku otrzymania akcji "login"
+        case 'login':
+
+            // $_POST['name'] 
+            $login = $_POST['loginInput'];
+            print_r($login);
+
+            $hashedPassword = hash('sha256', $_POST['passwordInput']);
+
+            $searchUser = "SELECT * FROM `users` WHERE login = '$login' LIMIT 1";
+            $search_result = $mysqli->query($searchUser) or die('Problem z bazą danych');
+
+            if ($rec = $search_result->fetch_array()) {
+
+                if ($hashedPassword == $rec['password']) {
+                    echo '<script type="text/javascript">console.log("poprawne hasło");</script>';
+
+                    if ($rec['isAdmin'] == 1) {
+
+                        $_SESSION['adminSession'] = true;
+                        header('location: admin/scores.php');
+                    } else {
+
+                        $_SESSION['userId'] = $rec['id'];
+                        header('location: user/scores.php');
+                    }
+                } else {
+
+                    echo '<script type="text/javascript">alert("Niepoprawne hasło")</script>';
+                    break;
+                }
+            } else {
+
+                echo '<script type="text/javascript">alert("Użytkownik nie istnieje")</script>';
+                break;
+            }
+            break;
+
+            // w przypadku otrzymania akcji "register"
+        case 'register':
+
+            $loginRegister = $_POST['loginInputRegister'];
+            $passwordRegister = $_POST['passwordInputRegister'];
+            $passwordRegisterValidate = $_POST['passwordInputRegisterValid'];
+
+            if (strlen($loginRegister) < 6) {
+
+                echo '<script type="text/javascript">alert("Login powinnien składać się z przynajmniej 6 znaków")</script>';
+                break;
+            }
+
+            if (strlen($passwordRegister) < 6) {
+
+                echo '<script type="text/javascript">alert("Hasło powinno składać się z przynajmniej 6 znaków")</script>';
+                break;
+            }
+
+            if ($passwordRegister !=  $passwordRegisterValidate) {
+                echo '<script type="text/javascript">alert("Hasła nie są takie same")</script>';
+                break;
+            }
+
+            $searchUser = "SELECT * FROM `users` WHERE login = '$loginRegister' LIMIT 1";
+            $search_result = $mysqli->query($searchUser) or die('Problem z bazą danych');
+
+            if ($search_result->fetch_array()) {
+
+                echo '<script type="text/javascript">alert("Nazwa użytkownika: ' . $loginRegister . ' jest zajęta")</script>';
+                break;
+            } else {
+
+                $hashedPassword = hash('sha256', $passwordRegister);
+                $addUser = "INSERT INTO `users` (login, password, isAdmin, correctCount, incorrectCount) VALUES ('$loginRegister', '$hashedPassword', 0,0,0)";
+
+                if ($mysqli->query($addUser) == true) {
+
+                    echo '<script type="text/javascript">alert("Konto: ' . $loginRegister . ' utworzone. Możesz się teraz zalogować")</script>';
+                    break;
+                }
+            }
+
+            break;
+    }
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="pl">
 
@@ -72,119 +188,6 @@
             </div>
         </div>
     </div>
-
-    <?php
-
-    //rozpoczęcie sesji php
-    session_start();
-
-    // połączenie z bazą danych
-    $mysqli = mysqli_connect('localhost', 'root', '', 'pytania');
-
-    //$mysqli->set_charset('utf8mb4');
-    echo '<script type="text/javascript">console.log("połączono z bazą danych");</script>';
-
-    // w przypadku sesji ustawionej na zmienną adminSession zwracamy panel admina
-    if (isset($_SESSION['adminSession'])) {
-        return header('location: admin/scores.php');
-    }
-
-    // w przypadku id użytkownika zwracamy stronę użytkownika
-    if (isset($_SESSION['userId'])) {
-        return header('location: user/scores.php');
-    }
-
-    // W przypadku zapytania post
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-        switch ($_GET['action']) {
-
-                // w przypadku otrzymania akcji "login"
-            case 'login':
-
-                // $_POST['name'] 
-                $login = $_POST['loginInput'];
-                print_r($login);
-
-                $hashedPassword = hash('sha256', $_POST['passwordInput']);
-
-                $searchUser = "SELECT * FROM `users` WHERE login = '$login' LIMIT 1";
-                $search_result = $mysqli->query($searchUser) or die('Problem z bazą danych');
-
-                if ($rec = $search_result->fetch_array()) {
-
-                    if ($hashedPassword == $rec['password']) {
-                        echo '<script type="text/javascript">console.log("poprawne hasło");</script>';
-
-                        if ($rec['isAdmin'] == 1) {
-
-                            $_SESSION['adminSession'] = true;
-                            header('location: admin/scores.php');
-                        } else {
-
-                            $_SESSION['userId'] = $rec['id'];
-                            header('location: user/scores.php');
-                        }
-                    } else {
-
-                        echo '<script type="text/javascript">alert("Niepoprawne hasło")</script>';
-                        break;
-                    }
-                } else {
-
-                    echo '<script type="text/javascript">alert("Użytkownik nie istnieje")</script>';
-                    break;
-                }
-                break;
-
-                // w przypadku otrzymania akcji "register"
-            case 'register':
-
-                $loginRegister = $_POST['loginInputRegister'];
-                $passwordRegister = $_POST['passwordInputRegister'];
-                $passwordRegisterValidate = $_POST['passwordInputRegisterValid'];
-
-                if (strlen($loginRegister) < 6) {
-
-                    echo '<script type="text/javascript">alert("Login powinnien składać się z przynajmniej 6 znaków")</script>';
-                    break;
-                }
-
-                if (strlen($passwordRegister) < 6) {
-
-                    echo '<script type="text/javascript">alert("Hasło powinno składać się z przynajmniej 6 znaków")</script>';
-                    break;
-                }
-
-                if ($passwordRegister !=  $passwordRegisterValidate) {
-                    echo '<script type="text/javascript">alert("Hasła nie są takie same")</script>';
-                    break;
-                }
-
-                $searchUser = "SELECT * FROM `users` WHERE login = '$loginRegister' LIMIT 1";
-                $search_result = $mysqli->query($searchUser) or die('Problem z bazą danych');
-
-                if ($search_result->fetch_array()) {
-
-                    echo '<script type="text/javascript">alert("Nazwa użytkownika: ' . $loginRegister . ' jest zajęta")</script>';
-                    break;
-                } else {
-
-                    $hashedPassword = hash('sha256', $passwordRegister);
-                    $addUser = "INSERT INTO `users` (login, password, isAdmin, correctCount, incorrectCount) VALUES ('$loginRegister', '$hashedPassword', 0,0,0)";
-
-                    if ($mysqli->query($addUser) == true) {
-
-                        echo '<script type="text/javascript">alert("Konto: ' . $loginRegister . ' utworzone. Możesz się teraz zalogować")</script>';
-                        break;
-                    }
-                }
-
-                break;
-        }
-    }
-
-    ?>
 
 </body>
 
